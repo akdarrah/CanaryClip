0d018c64870a936925180ff656c6f96a73c695a1
Adam Darrah <piremies@gmail.com>
2016-05-12 01:12:14 -0400

    Adding character to interface

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,13 +1,18 @@
 class Character < ActiveRecord::Base
-  has_many :schematics
+  has_many :schematics, dependent: :destroy
 
   validates :uuid, presence: true
   validates :uuid, uniqueness: true
 
   has_attached_file :avatar
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment :avatar,
+    content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
   after_create :schedule_api_worker
+
+  def to_s
+    (username || uuid)
+  end
 
   private
 
