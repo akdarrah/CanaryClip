132871f4bc06ab6e01c2e395a15d5e9b20efbfb1
Adam Darrah <piremies@gmail.com>
2016-09-04 22:33:46 -0400

    Format dimensions better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def dimensions(schematic)
+    width  = schematic.width
+    length = schematic.length
+    height = schematic.height
+    title  = "#{length} Length &times; #{width} Width &times; #{height} Height".html_safe
+
+    content_tag :span, title: title do
+      "#{length}L &times; #{width}W &times; #{height}H".html_safe
+    end
+  end
+
 end
