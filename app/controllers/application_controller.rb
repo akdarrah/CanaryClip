b2586f436b41549be5f19ebdb6ecc0678f652cf5
Adam Darrah <piremies@gmail.com>
2016-09-24 21:47:51 -0400

    current_character method is available to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_character
+
   rescue_from CanCan::AccessDenied do |exception|
     if current_user.nil?
       redirect_to new_user_session_path
