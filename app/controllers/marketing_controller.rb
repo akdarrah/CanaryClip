2a7396052dde4b95aa910a90682bd66a89b1797b
Adam Darrah <piremies@gmail.com>
2016-10-12 00:13:57 -0400

    Add getting started marketing button

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -4,6 +4,7 @@ class MarketingController < ApplicationController
     @marketing_render = Render.high_resolution.order('random()').first
     @schematics       = Schematic.public.limit(3)
     @characters       = Character.order('random()').limit(6)
+    @server           = Server.official
   end
 
 end
