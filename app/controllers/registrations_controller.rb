4c22116c94230914b56f2dc7bb9ffcd73a088da9
Adam Darrah <piremies@gmail.com>
2016-10-20 19:43:53 -0400

    Claim your character when signing up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,12 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    new_user_character_claim_path
+    @character_claim = resource.character_claims.first
+    user_character_claim_path(@character_claim)
+  end
+
+  def sign_up_params
+    params.require(:user)
+      .permit(:email, :password, :password_confirmation, {:character_claims_attributes => [:character_username]})
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :characters
-  has_many :character_claims
+  has_many :character_claims, inverse_of: :user
   has_many :owned_servers, foreign_key: :owner_user_id, class_name: "Server"
 
   belongs_to :current_character, class_name: "Character"
+
+  accepts_nested_attributes_for :character_claims
+
+  validates :character_claims, presence: true
 end
