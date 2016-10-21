3584c989e7f540f64ac739ad10f8fc9f9fbc602e
Adam Darrah <piremies@gmail.com>
2016-10-20 23:02:05 -0400

    Updated marketing to focus on builds more

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -2,7 +2,7 @@ class MarketingController < ApplicationController
 
   def index
     @marketing_render = Render.high_resolution.order('random()').first
-    @schematics       = Schematic.published.order('random()').limit(3)
+    @schematic        = Schematic.published.order('random()').first
     @server           = Server.official
   end
 
