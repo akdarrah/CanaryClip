4820a0ea118ef224e7e6d49d8d8864e482ef0e69
Adam Darrah <piremies@gmail.com>
2016-10-25 20:02:41 -0400

    Add state to schematic edit view

---
+++ app/helpers/schematics_helper.rb
@@ -0,0 +1,37 @@
+module SchematicsHelper
+
+  def schematic_state_label_class(state)
+    case state
+    when 'new'
+      'label-default'
+    when 'collecting_metadata'
+      'label-info'
+    when 'rendering'
+      'label-warning'
+    when 'published'
+      'label-primary'
+    end
+  end
+
+  def schematic_state_description(state)
+    case state
+    when 'new'
+      content_tag :div, class: "alert alert-warning" do
+        "This build is currently waiting to be analyzed."
+      end
+    when 'collecting_metadata'
+      content_tag :div, class: "alert alert-warning" do
+        "This build is currently being analyzed before it can be rendered."
+      end
+    when 'rendering'
+      content_tag :div, class: "alert alert-warning" do
+        "This build is being rendered&hellip; Rendering usually takes 10-15 minutes.".html_safe
+      end
+    when 'published'
+      content_tag :div, class: "alert alert-success" do
+        "#{fa_icon('check-circle ')} This build has been published and is discoverable by other users.".html_safe
+      end
+    end
+  end
+
+end
