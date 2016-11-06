7356e6de4c5f4c4895e12914b0eaa85c24a0202e
Adam Darrah <piremies@gmail.com>
2016-11-06 01:47:31 -0500

    Revert "Revert "Another attempt to fix downloads from s3""
    This reverts commit d533849fbe6fe110b48670f6c00e60b2f6d598e7.

--- app/controllers/schematics_controller.rb
+++ app/controllers/schematics_controller.rb
@@ -33,8 +33,11 @@ class SchematicsController < ApplicationController
   end
 
   def download
-    data = open(@schematic.file.url)
-    send_data data.read, :filename => @schematic.file_file_name, :type => @schematic.file_content_type, :x_sendfile => true
+    send_file(Paperclip.io_adapters.for(@schematic.file).path, {
+      :type        => @schematic.file_content_type,
+      :disposition => "attachment",
+      :filename    => @schematic.file_file_name
+    })
   end
 
   private
